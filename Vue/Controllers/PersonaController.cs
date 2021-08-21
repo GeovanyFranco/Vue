2481b5d363438e792f4893f044a4498074697349
Geokiller <63886424+GeovanyFranco@users.noreply.github.com>
2021-08-20 22:55:06 -0600

    Con consulta Ajax

---
+++ Vue/Controllers/PersonaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vue.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+
+        [HttpGet]
+        public Persona Get()
+        {
+            Persona oPersona = new Persona() { Nombre = "Geovanny" };
+
+            return oPersona;
+        }
+    }
+
+    public class Persona
+    {
+        public string Nombre { get; set; }
+    }
+}
